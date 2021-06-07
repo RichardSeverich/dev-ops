467d180d4d1e1ceecd7db937b5d5b15615f2b2fe
“RichardSeverich” <richard.severich@jalasoft.com>
2021-06-07 18:33:40 -0400

    add terraform variables

---
+++ terraform/example-003/variables.tf
@@ -0,0 +1,43 @@
+variable "instance_count" {
+  default = 2
+}
+
+variable "user_name" {
+  type = string
+  sensitive = true
+}
+
+variable "user_password" {
+  type = string
+  sensitive = true
+}
+
+variable "image" {
+  default = "ubuntu-20.04-server_20210415"
+  type = string
+}
+
+variable "flavor" {
+  default = "server.v1_2.4R"
+  type = string
+}
+
+variable "ssh_key_file" {
+  default = "./../example-002/devops_rsa.pub"
+  type = string
+}
+
+variable "instance_prefix" {
+  default = "VMRICH"
+  type = string
+}
+
+variable "key_pair" {
+  default = "kprise01"
+  type = string
+}
+
+variable "security_group" {
+  default = "sgrise01"
+  type = string
+}
