e80c2ae5d4481d33089c6d5e9b8bd1730d485e0e
“RichardSeverich” <richard.severich@jalasoft.com>
2021-06-07 22:04:34 -0400

    Add volumes

---
+++ terraform/example-004-volumes/main.tf
@@ -0,0 +1,73 @@
+# Define required providers
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.41.0"
+    }
+  }
+}
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_domain_name = "jalasoft"
+  user_name   = var.user_name
+  password    = var.user_password
+  auth_url    = "https://10.24.1.230:5000/v3"
+  tenant_id   = "ec2e02a3c58b4a689fe62be31b3f4a87"
+  insecure    = true
+  region      = "jala-hq"
+}
+# Create Key Pair
+resource "openstack_compute_keypair_v2" "key_pair" {
+  name = "${var.key_pair}"
+  public_key = file("${var.ssh_key_file}")
+}
+# Create Security Group
+resource "openstack_compute_secgroup_v2" "secgroup_1" {
+  name = "${var.security_group}"
+  description = "Richard security group"
+  rule {
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    from_port = 3389
+    to_port = 3389
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    from_port = 80
+    to_port = 80
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    from_port = -1
+    to_port = -1
+    ip_protocol = "icmp"
+    cidr = "0.0.0.0/0"
+  }
+}
+# Create server
+resource "openstack_compute_instance_v2" "dev_server" {
+  name            = var.instance_name
+  image_name      = var.image
+  flavor_name     = var.flavor
+  key_pair        = openstack_compute_keypair_v2.key_pair.name
+  security_groups = ["default", "${openstack_compute_secgroup_v2.secgroup_1.name}"]
+}
+# Volumes
+resource "openstack_blockstorage_volume_v2" "volume_1" {
+  name              = var.volume_name
+  description       = "Richard volume"
+  size              = 1
+}
+
+resource "openstack_compute_volume_attach_v2" "attached" {
+  instance_id = "${openstack_compute_instance_v2.dev_server.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.volume_1.id}"
+}
